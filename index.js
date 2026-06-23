86203621121ac7b5de1a540b8939088a9332f9be
mroscardd <mroscardd@gmail.com>
2026-06-23 15:50:29 -0400

    auth

--- index.js
+++ index.js
@@ -18,9 +18,11 @@ app.use("/customer/auth/*", function auth(req,res,next){
                 req.user = user
                 next()
             } else {
-                return res.status(403).json('')
+                return res.status(403).json({ message: "User not authenticated" })
             }
         })
+    } else {
+        return res.status(403).json({ message: "User not logged in" })
     }
 
 });
