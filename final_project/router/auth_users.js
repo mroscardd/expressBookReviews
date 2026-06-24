16decd57431b87ceb65dbb2523270c1d3a7de634
mroscardd <mroscardd@gmail.com>
2026-06-24 20:03:52 +0200

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -15,15 +15,49 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username
+  const password = req.body.password
+  if (!username || !password) {
+    return res.status(404).json({message: "Provide username and password"})
+  }
+
+  const valid_user = users.filter(user => user.username == username)
+
+  if (valid_user.length > 0 ) {
+    if (valid_user[0].password == password) {
+        let accessToken = jwt.sign({
+            data: username
+        }, 'access', { expiresIn: 60 * 60 });
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
+  }
+  return res.status(404).json({message: "This user is not registered"})
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  const isbn = req.params.isbn
+  const data = req.body.data
+  const user = req.user.data
+  books[isbn]['reviews'] = {
+    ...books[isbn]['reviews'],
+    [user]: {'content': data}    
+    }
+    return res.status(200).json(books[isbn]['reviews'])  
+  })
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {  
+    const isbn = req.params.isbn
+    const user = req.user.data
+    delete books[isbn]['reviews'][user]
+    return res.status(200).json({message: "Review delete"})
+})
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
